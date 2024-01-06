a7eb3b463c7df8bf19bbc036bc07b51dad37d792
Lernur <143444796+Lernur@users.noreply.github.com>
2024-01-06 21:02:15 +0300

    Add files via upload

--- task7_1.py
+++ task7_1.py
@@ -3,27 +3,28 @@ import random
 
 def check_input(a):
     if len(a) == 2 and isinstance(a[0], int) and isinstance(a[1], int):
-        if int(a[0]) < int(a[1]):
+        if 0 < int(a[0]) and int(a[0]) < int(a[1]):
             return True
     return False
 
 
-def get_ladder(a, b):
+def get_ladder(c):
+    a = int(c[0])
+    b = int(c[1])
     n = random.randint(a, b)
     c = []
     for i in range(b, n-1, -1):
-        c.append(i)
+        c.append(i*"*")
     for i in range(a, n+1):
-        c.append(i)
+        c.append(i*"*")
     return c
 
 
 def main(c):
     for i in c:
         if check_input(i):
-            a = int(i[0])
-            b = int(i[1])
-            c = get_ladder(a, b)
+            c = get_ladder(i)
             for j in c:
-                print("*"*int(j))
+                print(j)
     return
+
